776b4dd5e9c84038108f0993afd66e3324dd4ca7
fc8880583 <fc8880583@gmail.com>
2018-02-13 21:45:46 -0600

    giaguode

--- App1/App1/MainPage.xaml.cs
+++ App1/App1/MainPage.xaml.cs
@@ -7,11 +7,45 @@ using Xamarin.Forms;
 
 namespace App1
 {
-	public partial class MainPage : ContentPage
-	{
+    public partial class MainPage : ContentPage
+    {
+        private Button[] b = new Button[6]; 
 		public MainPage()
 		{
 			InitializeComponent();
+            b[0] = ah;
+            b[1] = fw;
+            b[2] = oa;
+            b[3] = s;
+            b[4] = e;
+            b[5] = ct;
 		}
-	}
+        private void Ahcd(object sender, EventArgs e)
+        {
+           // b[0].BackgroundColor = Color.Red;
+        }
+        private void Fwcd(object sender, EventArgs e)
+        {
+            Console.Write(3);
+        }
+        private void  Oacd(object sender, EventArgs e)
+        {
+            Console.Write(3);
+        }
+        private void Scd(object sender, EventArgs e)
+        {
+            Console.Write(3);
+        }
+        private void Ecd(object sender, EventArgs e)
+        {
+            Console.Write(3);
+        }
+
+        private void Ctcd(object sender, EventArgs e)
+        {
+            Console.Write(3);
+        }
+    }
+   
+    
 }
