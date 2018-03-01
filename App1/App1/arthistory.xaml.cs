2b78f6cc8153836c2f79624de5bb8eac52c85a38
fc8880583 <fc8880583@gmail.com>
2018-02-28 22:50:08 -0600

    all view

---
+++ App1/App1/arthistory.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class arthistory : ContentPage
+	{
+        private Button[] b = new Button[5];
+        public arthistory ()
+		{
+			InitializeComponent ();
+
+            b[0] = MV;
+            b[1] = EX;
+            b[2] = SH;
+            b[3] = ART;
+            b[4] = SC;
+        }
+	}
+}
